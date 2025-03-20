6e4e7a8dcdc79e6993521b96bb437584d1f5f4c2
gifcha <rudolfsbriedis77@gmail.com>
2025-03-20 13:20:46 +0200

    started work on game tree

--- gametree.py
+++ gametree.py
@@ -1,4 +1,16 @@
 
 class node:
-    connectedNodes = None
+    connectedNodes = []
     value: list[int] = []
+
+    def __init__(self, value: list[int]):
+        self.value = value
+
+    def addChildNode(self, node):
+        self.connectedNodes.append(node)
+
+
+
+def createGameTree(value: list[int]):
+    root = node(value)
+    # add all states using addChildNode(node(statevalue))
