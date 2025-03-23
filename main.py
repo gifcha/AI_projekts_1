17ffe3be9e2b8300d9bf57fb66fb3acfe7d9511d
plakitins@gmail.com <plakitins@gmail.com>
2025-03-23 14:01:45 +0200

    some-changes

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import random as rd
 from gui import start_game
+from heur_func import heuristic_function
+from gametree import createGameTree
 
-start_game()
+# GUI funkcija
+# start_game()
+
+# Skaitļu virkne
+defaultLine = ""
+lineLength = int(input("Choos a number between form 15 till 25 : "))
+for x in range(0,lineLength):
+    a = rd.randint(1,9)
+    defaultLine = defaultLine + str(a)
+
+print(defaultLine)
+
+# Šeit vajag funkciju kura ģenerēs koku
+
+# Šeit vajag ciklu kurš katru virsotni ar iesūtīs funkcijā "heuristic_function()" un tā piešķirs virsotnei svaru
