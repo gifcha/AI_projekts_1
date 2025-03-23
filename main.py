d595bf3b980453c5e3ad26f8e848c1e1c9f7c4f5
plakitins@gmail.com <plakitins@gmail.com>
2025-03-23 14:14:47 +0200

    changes

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from gametree import createGameTree
 # start_game()
 
 # Skaitļu virkne
-startLine = ""
+startString = ""
 lineLength = int(input("Choos a number between form 15 till 25 : "))
 for x in range(0,lineLength):
     a = rd.randint(1,9)
-    startLine = startLine + str(a)
+    startString = startString + str(a)
 
-print(startLine)
+print(startString)
 
-# Šeit vajag funkciju kura ģenerēs koku pamatojoties uz virkni startLine
+# Šeit vajag funkciju kura ģenerēs koku pamatojoties uz virkni startString
 
 # Šeit vajag ciklu kurš katru virsotni iesūtīs funkcijā "heuristic_function()" un tā piešķirs virsotnei svaru
